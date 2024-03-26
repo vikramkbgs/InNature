52747476d1492d4c63b1cf94eafef53d238c3868
vikramkbgs <vikramkbgs@gmail.com>
2024-03-26 22:15:37 +0530

    adding user and campground routes

---
+++ routes/users.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const catchAsync = require('../utils/catchAsync');
+const User = require('../models/user');
+const {storeReturnTo} = require('../middleware');
+
+router.get('/register',(req, res) =>{
+    res.render('users/register');
+});
+
+router.post('/register', catchAsync(async(req, res, next) =>{
+    try{
+        const {email, username, password} = req.body;
+        const user = new User({email, username});
+        const registeredUser = await User.register(user, password);
+
+        //auto login 
+        req.login(registeredUser, err =>{
+            if(err) return next(err);
+            req.flash('sucess', 'Welcome to InNature!');
+            res.redirect('/campgrounds');
+        })
+    }catch(e){
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}));
+
+router.get('/login',(req, res) =>{
+    res.render('users/login');
+})
+
+router.post('/login',
+    // use the storeReturnTo middleware to save the returnTo value from session to res.locals
+    storeReturnTo,
+    // passport.authenticate logs the user in and clears req.session
+    passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
+    // use res.locals.returnTo to redirect the user after login
+    (req, res) => {
+        req.flash('success', 'Welcome back!');
+        const redirectUrl = res.locals.returnTo || '/campgrounds';
+        res.redirect(redirectUrl);
+    });
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+});
+
+module.exports = router;
