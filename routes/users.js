c362c28663c092daa67f3271ec03eae0d6674504
vikramkbgs <vikramkbgs@gmail.com>
2024-04-03 20:45:55 +0530

    Adding controllers and config with routes

--- routes/users.js
+++ routes/users.js
@@ -3,54 +3,17 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
-const {storeReturnTo} = require('../middleware');
+const { storeReturnTo } = require('../middleware');
+const users = require('../controllers/users');
 
-router.get('/register',(req, res) =>{
-    res.render('users/register');
-});
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(async(req, res, next) =>{
-    try{
-        const {email, username, password} = req.body;
-        const user = new User({email, username});
-        const registeredUser = await User.register(user, password);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
-        //auto login 
-        req.login(registeredUser, err =>{
-            if(err) return next(err);
-            req.flash('sucess', 'Welcome to InNature!');
-            res.redirect('/campgrounds');
-        })
-    }catch(e){
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
-}));
-
-router.get('/login',(req, res) =>{
-    res.render('users/login');
-})
-
-router.post('/login',
-    // use the storeReturnTo middleware to save the returnTo value from session to res.locals
-    storeReturnTo,
-    // passport.authenticate logs the user in and clears req.session
-    passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
-    // use res.locals.returnTo to redirect the user after login
-    (req, res) => {
-        req.flash('success', 'Welcome back!');
-        const redirectUrl = res.locals.returnTo || '/campgrounds';
-        res.redirect(redirectUrl);
-    });
-
-router.get('/logout', (req, res, next) => {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success', 'Goodbye!');
-        res.redirect('/campgrounds');
-    });
-});
+router.get('/logout', users.logout)
 
 module.exports = router;
